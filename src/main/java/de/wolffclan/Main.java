3b3d04d343834a5c073703902fefc48ab1f3d9e3
JoergWolff <neuerfisch@wolffclan.de>
2023-09-07 11:13:50 +0200

    added step 1-4

---
+++ src/main/java/de/wolffclan/Main.java
@@ -0,0 +1,44 @@
+package de.wolffclan;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Stream\n------------");
+
+        List<Integer> ints = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            ints.add(i);
+        }
+        System.out.println(ints);
+        System.out.println("Nur gerade Zahlen \n------------");
+        ints.stream()
+                .filter(number -> number % 2 == 0)
+                .forEach(System.out::println);
+
+        System.out.println("Zahlen verdoppeln \n------------");
+        ints.stream()
+                .map(number -> number*2)
+                .forEach(System.out::println);
+
+        System.out.println("Zahlen rückwärts \n------------");
+        ints.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
+
+        System.out.println("Zahlen summieren \b------------");
+
+        Optional<Integer> sumComplete = ints.stream()
+                .reduce(Integer::sum);
+        if(sumComplete.isPresent()){
+            System.out.println("Summe" + sumComplete.get());
+        }
+        else{
+            System.out.println("Kein Wert vorhanden");
+        }
+
+    }
+}
